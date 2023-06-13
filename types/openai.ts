0ff6bc8e8a514051db30c97b8117ba2f2450480b
Jmackles <124995231+Jmackles@users.noreply.github.com>
2023-06-13 14:22:36 -0400

    (Hopefully) added the 16k 3.5 model support. Idefk

--- types/openai.ts
+++ types/openai.ts
@@ -9,7 +9,7 @@ export interface OpenAIModel {
 
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_AZ = 'gpt-35-turbo',
+  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
 }
@@ -25,8 +25,8 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     tokenLimit: 4000,
   },
   [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
+    id: OpenAIModelID.GPT_3_5_16K,
+    name: 'GPT-3.5-16K',
     maxLength: 12000,
     tokenLimit: 4000,
   },
